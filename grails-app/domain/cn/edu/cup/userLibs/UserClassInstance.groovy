94d8ce2c4ddacb0877019ff34c70f027d0b5b6ca
Win10Lxp <xplixina@sina.com>
2016-05-26 12:13:37 +0800

    创建类的实例

--- grails-app/domain/cn/edu/cup/userLibs/UserClassInstance.groovy
+++ grails-app/domain/cn/edu/cup/userLibs/UserClassInstance.groovy
@@ -12,4 +12,17 @@ class UserClassInstance {
     String toString() {
         return "${name}"
     }
+    
+    /*
+     * 创建类的实例
+     * */
+    def classInstance() {
+        ClassLoader parent = getClass().getClassLoader(); 
+        GroovyClassLoader loader = new GroovyClassLoader(parent);  
+        def libFile = new File(lib.realFileName())
+        loader.addURL(libFile.toURL())
+        def clazz = loader.parseClass(name)
+        def object = clazz.newInstance()
+        return object
+    }
 }
